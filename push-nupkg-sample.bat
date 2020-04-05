0d631855f495b0b6f6434e4b26a6a547d773798d
DUONG Phu-Hiep <duongphuhiep@gmail.com>
2020-04-05 21:38:50 +0200

    add --skip-duplicate

--- push-nupkg-sample.bat
+++ push-nupkg-sample.bat
@@ -1,13 +1,2 @@
-dotnet nuget push ToolsPack.Config.*.nupkg -k secretkey -s https://www.nuget.org/api/v2/package
-dotnet nuget push ToolsPack.Displayer.*.nupkg -k secretkey -s https://www.nuget.org/api/v2/package
-dotnet nuget push ToolsPack.Log4net.*.nupkg -k secretkey -s https://www.nuget.org/api/v2/package
-dotnet nuget push ToolsPack.NLog.*.nupkg -k secretkey -s https://www.nuget.org/api/v2/package
-dotnet nuget push ToolsPack.Samba.*.nupkg -k secretkey -s https://www.nuget.org/api/v2/package
-dotnet nuget push ToolsPack.Sql.*.nupkg -k secretkey -s https://www.nuget.org/api/v2/package
-dotnet nuget push ToolsPack.String.*.nupkg -k secretkey -s https://www.nuget.org/api/v2/package
-dotnet nuget push ToolsPack.Thread.*.nupkg -k secretkey -s https://www.nuget.org/api/v2/package
-dotnet nuget push ToolsPack.Webservice.*.nupkg -k secretkey -s https://www.nuget.org/api/v2/package
-
-
-dotnet nuget push *.nupkg -k secretkey -s https://www.nuget.org/api/v2/package
+dotnet nuget push *.nupkg -k secret -s https://www.nuget.org/api/v2/package --skip-duplicate
 
