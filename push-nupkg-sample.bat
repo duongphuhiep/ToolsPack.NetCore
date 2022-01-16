73cb1291a043983d76f9f8102baacda083c6c84f
DUONG Phu-Hiep <hduong@lemonway.com>
2022-01-16 23:35:06 +0100

    update batch push samples

--- push-nupkg-sample.bat
+++ push-nupkg-sample.bat
@@ -1,2 +1,5 @@
-dotnet nuget push *.nupkg -k secret -s https://www.nuget.org/api/v2/package --skip-duplicate
+set FILES=*.nupkg
 
+for %%f in (%FILES%) do (
+	dotnet nuget push %%~f -k secret -s https://www.nuget.org/api/v2/package --skip-duplicate
+)
